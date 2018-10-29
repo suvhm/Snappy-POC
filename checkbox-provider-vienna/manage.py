a82c387c45d218af4c1f721551214dcf943a8b15
Shubham Pushpkar <shubham2.pushpkar@aricent.com>
2018-10-29 16:11:27 +0530

    checkbox-vienna

---
+++ checkbox-provider-vienna/manage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup, N_
+
+setup(
+    name='checkbox-provider-vienna',
+    namespace='com.canonical.vienna',
+    version="0.1",
+    description=N_(
+        "Checkbox Provider for QA of devices in the Vienna project"),
+    gettext_domain='checkbox-provider-vienna',
+)
